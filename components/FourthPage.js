87af6c4d6179d3c6e29a1d729376c888ff78d18f
Adnan Gondal <adnangondal1@hotmail.com>
2022-02-08 20:41:29 +0000

    a few more changes

--- components/FourthPage.js
+++ components/FourthPage.js
@@ -2,10 +2,72 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import { Button, TouchableOpacity } from 'react-native-web';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import styled from 'styled-components/native'
+
+
+const Container = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`
+
+const Header = styled.Text`
+    color: #000;
+    font-size: 30px;
+    margin-bottom: 30px;
+`
+
+const CardContainer = styled.View`
+    width: 90%;
+    max-width: 260px;
+    height: 300px;
+`
+
+const Card = styled.View`
+    position: absolute;
+    background-color: #fff;
+    width: 100%;
+    max-width: 260px;
+    height: 300px;
+    shadow-color: black;
+    shadow-opacity: 0.2;
+    shadow-radius: 20px;
+    border-radius: 20px;
+    resize-mode: cover;
+`
+
+const CardImage = styled.ImageBackground`
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-radius: 20px;
+`
+
+const CardTitle = styled.Text`
+    position: absolute;
+    bottom: 0;
+    margin: 10px;
+    color: #fff;
+`
+
+const Buttons = styled.View`
+    margin: 20px;
+    z-index: -100;
+`
+
+const InfoText = styled.Text`
+    height: 28px;
+    justify-content: center;
+    display: flex;
+    z-index: -100;
+`
 
 export default function FourthPage({route, navigation}) {
-    const { groupID,code } = route.params;
+  
+    const { groupID } = route.params;
+
     const [data, setData] = useState([]);
 
     const getResults = async () => {
@@ -25,6 +87,8 @@ export default function FourthPage({route, navigation}) {
         getResults();
       }, []);
 
+
+
   return (
     <LinearGradient
     // Background Linear Gradient
@@ -32,8 +96,9 @@ export default function FourthPage({route, navigation}) {
     style={styles.background}
   >
     <View style={styles.container}>
-      <Text> WINNER: {data[0].title}</Text>
-      <Text> Votes: {data[0].votes}</Text>
+      <Text> WINNER: {data[0] ? data[0].title : "NOT FOUND"} </Text>
+
+     
     </View>
     </LinearGradient>
   );
